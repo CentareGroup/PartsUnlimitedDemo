bef98ae0990b814b7b5a5b2681230a462d9b8d02
Dan Piessens <dan.piessens@centare.com>
2015-08-05 16:29:39 -0400

    Added nuget packaging

---
+++ env/OctopusDeploy/PublishProject.ps1
@@ -0,0 +1,26 @@
+﻿param ([string]$OutputDirectory,
+       [string]$BuildNumber)
+
+
+# Download a copy of NuGet if needed
+$CachedNuGetDir = Join-Path $env:APPDATA 'NuGet'
+$CachedNuGetFile = Join-Path $CachedNuGetDir 'NuGet.exe'
+
+If (!(Test-Path $CachedNuGetFile)) {
+    New-Item -Path $CachedNuGetDir -ItemType Directory | Out-Null
+    Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile $CachedNuGetFile
+}
+
+$localNuGetExe = '.nuget\nuget.exe'
+If (!(Test-Path $localNuGetExe)) {
+    New-Item -Path '.nuget' -ItemType Directory -Force | Out-Null
+    Copy-Item -Path $CachedNuGetFile -Destination $localNuGetExe
+}
+
+# Use NuGet to pack up the website
+Write-Host "Packaging up project"
+
+$basePath = Resolve-Path -Path '.\src\PartsUnlimitedWebsite'
+& $localNuGetExe pack src\PartsUnlimitedWebsite\PartsUnlimited.Web.nuspec -OutputDirectory $OutputDirectory -BasePath $basePath -Version "1.0.1.$BuildNumber" -Properties Configuration=Release -Properties id=PartsUnlimited.Web
+
+# Use NuGet to pull down  
