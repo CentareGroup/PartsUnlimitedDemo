96189e7de229158f79f420c1dfc2acabd57e7e21
Dan Piessens <dan.piessens@centare.com>
2015-08-06 11:03:29 -0400

    Added debug variables and fixed bad configuration variable

--- env/OctopusDeploy/PublishProject.ps1
+++ env/OctopusDeploy/PublishProject.ps1
@@ -30,6 +30,6 @@ If (!(Test-Path $localNuGetExe)) {
 Write-Host "Packaging up project"
 
 $basePath = Resolve-Path -Path '.\src\PartsUnlimitedWebsite'
-& $localNuGetExe pack src\PartsUnlimitedWebsite\PartsUnlimited.Web.nuspec -OutputDirectory $OutputDirectory -BasePath $basePath -Version $BuildNumber -Properties "Configuration=$Configuration" -Properties id=PartsUnlimited.Web
+& $localNuGetExe pack src\PartsUnlimitedWebsite\PartsUnlimited.Web.nuspec -OutputDirectory $OutputDirectory -BasePath $basePath -Version $BuildNumber -Properties Configuration=${Configuration} -Properties id=PartsUnlimited.Web
 
 # Use NuGet to pull down  
