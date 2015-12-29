3bb9c624e214d6a910154453d53144a126f67857
Dan Piessens <dan.piessens@centare.com>
2015-12-29 13:53:31 -0600

    Added nuspec files for DB deployment.

---
+++ src/PartsUnlimited.Models/Deployment/Deploy.ps1
@@ -0,0 +1,18 @@
+﻿
+# These variables should be set via the Octopus web portal:
+#
+#   ConnectionString         - The .Net connection string for the DB
+ 
+
+Write-Host "Connection String: <"$ConnectionString">"
+
+# Get the exe name based on the directory
+$contentPath  = $OctopusOriginalPackageDirectoryPath
+$fullPath = (Join-Path $contentPath "migrate.exe")
+Write-Host "Migrate Path:" $fullPath
+
+cd $contentPath
+write-host "Working Dir: "$(get-location)
+
+# Run the migration utility
+& ".\migrate.exe" PartsUnlimited.Models.dll /startUpConfigurationFile=PartsUnlimited.Models.dll.config /connectionString=$ConnectionString /connectionProviderName="System.Data.SqlClient" /verbose | Write-Host
