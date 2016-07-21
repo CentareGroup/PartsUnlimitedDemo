085968f21ddba48cbfe9e4ed4db70bfa3cfdcec1
tyler.evert <tyler.evert@centare.com>
2016-07-21 15:55:04 -0500

    1-upping Dan's version

--- MRP/deploy/Chef/cookbooks/mrpapp-idempotent/metadata.rb
+++ MRP/deploy/Chef/cookbooks/mrpapp-idempotent/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures mrp application server'
 long_description 'Installs/Configures mrp application server'
-version '0.2.3'
+version '0.3.1'
 
 depends 'apt'
 depends 'tomcat', '~> 2.3.1'
