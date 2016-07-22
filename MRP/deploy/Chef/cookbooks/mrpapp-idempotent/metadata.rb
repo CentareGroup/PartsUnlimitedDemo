b6c2ef89cea8d62711110e4889176085a88807bf
tyler.evert <tyler.evert@centare.com>
2016-07-22 10:52:43 -0500

    Bash syntax is hard

--- MRP/deploy/Chef/cookbooks/mrpapp-idempotent/metadata.rb
+++ MRP/deploy/Chef/cookbooks/mrpapp-idempotent/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures mrp application server'
 long_description 'Installs/Configures mrp application server'
-version '0.4.7'
+version '0.4.8'
 
 depends 'apt'
 depends 'java'
