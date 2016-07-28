ff2026ecec96a74a26e2ab24be6b59cc746df012
Dan Piessens <dan.piessens@gmail.com>
2016-07-28 10:49:25 -0400

    Updating remove pattern

--- env/OctopusSharedSteps/DeleteResourceGroup.ps1
+++ env/OctopusSharedSteps/DeleteResourceGroup.ps1
@@ -1,12 +1,18 @@
 ﻿$resourceGroupName = $OctopusParameters['Deployment.ResourceGroupName']
 $resourceGroupLocation = $OctopusParameters['Deployment.ResourceGroupLocation']
+$doNotDelete = [System.Convert]::ToBoolean($OctopusParameters['Deployment.DoNotRemove'])
+
+If ($doNotDelete) {
+    Write-Host "Do not delete requested, skipping"
+    Return
+}
 
 # Try getting the group first
 $group = Get-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -ErrorAction SilentlyContinue
 If ($group -ne $null)
 {
     Write-Host "Removing resource group '$resourceGroupName' in $resourceGroupLocation"
-    $group | Remove-AzureRmResourceGroup -Confirm -Pre -Force
+    $group | Remove-AzureRmResourceGroup -Confirm -Force
     Write-Host "Group removed successfully!"
 }
 Else
